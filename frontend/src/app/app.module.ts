36d5f7ff85517678037bbb6d215612bd8e3d94e7
BrendanSD3 <brendan.wall03@gmail.com>
2020-12-21 18:06:15 +0000

    Routing and pages setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { PageTwoComponent } from './page-two/page-two.component';
 import { PageOneComponent } from './page-one/page-one.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SongsComponent } from './songs/songs.component';
 
 const appRoutes: Routes = [
   { path: 'page-one', component: PageOneComponent },
   { path: 'page-two', component: PageTwoComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'songs', component: SongsComponent },
   { path: '', redirectTo: '/page-one', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -20,6 +26,9 @@ const appRoutes: Routes = [
     PageOneComponent,
     PageTwoComponent,
     PageNotFoundComponent,
+    AboutComponent,
+    HomeComponent,
+    SongsComponent,
     
   ],
   imports: [
